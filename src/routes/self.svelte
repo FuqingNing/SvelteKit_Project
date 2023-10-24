099dd7fed0974df6c36565ba0c0ad140c54a1fd0
FuqingNing <87643230+FuqingNing@users.noreply.github.com>
2023-10-24 22:43:20 +1100

    commit

---
+++ src/routes/self.svelte
@@ -0,0 +1,37 @@
+<script lang = "ts">
+
+import { onMount } from 'svelte';
+import * as PIXI from 'pixi.js';
+
+let app: PIXI.Application;
+interface Cell{
+    isHidden: boolean;
+      isBomb: boolean;
+      hasFlag: boolean; // Use camelCase for property names
+      bombsNearby: number; // It seems like a typo: 'bombNearBy' should be 'bombsNearby'
+      id: number;
+}
+interface Game {
+      width: number;
+      height: number;
+      bombsCount: number;
+      cells: Cell[]
+    }
+      
+      onMount(()=>{
+        app = new PIXI.Application({
+          width: 800,
+          height: 600,
+          backgroundColor: 0xEEEEEE,
+        });
+      });
+const cell = new PIXI.Graphics();
+cell.beginFill(0x000000);
+cell.drawRect(0, 0, 100, 100);
+cell.endFill();
+
+</script>
+
+<style>
+
+</style>
